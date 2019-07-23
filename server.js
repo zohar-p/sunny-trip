d89eba2b6522d4c802e6264435f93cbd029cb824
DESKTOP-3DN4RNO\idank <idankcompany@gmail.com>
2019-07-23 11:16:41 +0300

    server setup

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const app = express()
 const api = require('./server/routes/api')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+const data = require('./city-to-code.json')
+const cityCode = require('./server/models/City-Code')
 const path = require('path')
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/', {useNewUrlParser: true}) // add DB name
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/sunnytrip', {useNewUrlParser: true})
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
@@ -13,6 +15,17 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
 app.use('/', api)
+
+
+
+//Load all data to DB
+// const loadAll = function () {
+//     for (item of data) {
+//         new cityCode ( {city: item.city.replace("-"," ").toLowerCase(), airportCode: item.airportCode} ).save();
+//     }
+// }
+
+// loadAll();
 
 const port = process.env.PORT || 3000
 app.listen(port, ()=>console.log('Server running on port ' + port))
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,5 +1,26 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const CityCode = require('../models/City-Code')
+const request = require('request-promise-native');
+const APIKEY = "";
 
 
-module.exports = router
+router.get("/flights/:fromCity/:fromDate/:toDate/:fromTemp/:toTemp", async function (req, res) {
+    const reqParams = {
+        fromCity : req.params.fromCity.replace("-"," "),
+        fromDate : req.params.fromDate,
+        toDate : req.params.toDate,
+        fromTemp : req.params.fromTemp,
+        toTemp : req.params.toTemp,
+        maxPrice : req.query.maxPrice,
+        dur : req.query.dur
+    }
+
+    const cityCodes = await CityCode.find({city: reqParams.fromCity}, { airportCode : 1 });
+    res.send(cityCodes);
+
+    //const flightResult = await request(`https://api.skypicker.com/flights?flyFrom=${reqParams.fromCity}`);
+
+})
+
+module.exports = router;
