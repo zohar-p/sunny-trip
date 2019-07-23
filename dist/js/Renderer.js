8d181eb379c5289d1bac7e5bd76949c579e9a3c6
liorho <hoshealior@gmail.com>
2019-07-23 19:33:16 +0300

    added saved

--- dist/js/Renderer.js
+++ dist/js/Renderer.js
@@ -1,37 +1,40 @@
-class Renderer{
+class Renderer {
 
-    renderSearchResults(data){
+    renderSearchResults(data) {
         const source = $('#flights-template').html();
         const template = Handlebars.compile(source);
-        const newHTML = template({data});
-        $("#container-results").empty();
-        $("#container-results").append(newHTML);
-    }
-    
-    renderSavedSearches(data){
-        const source = $('#savedSearch-template').html();
-        const template = Handlebars.compile(source);
-        const newHTML = template({data});
+        const newHTML = template({ data });
         $("#container-results").empty();
         $("#container-results").append(newHTML);
     }
 
-    renderEmptyInput(input){
+    renderSavedSearches(data) {
+        const source = $('#savedSearch-template').html();
+        const template = Handlebars.compile(source);
+        const newHTML = template({ data });
+        $("#container-results").empty();
+        $("#container-results").append(newHTML);
+    }
+
+    renderEmptyInput(input) {
         input.css("border", "red")
     }
 
-    renderNoResults(){
+    renderNoResults() {
         let noResults = `<div class="no-result">SORRY, NO RESULTS FOUND<div>`
         $("#container-results").append(noResults);
     }
 }
 // CHECK
 const render = new Renderer
+
+render.renderNoResults()
+
 // let a = [{
 //     cityName: "Tel Aviv",
 //     date: "01/01/2019",
 //     price: 34,
-//     flightDuration:4.6,
+//     flightDuration: 4.6,
 //     temp: 30,
 //     condition: "cloudy",
 //     conditionPic: "https://cdn1.iconfinder.com/data/icons/airplane-glyph/64/Airplane_Flight_Airport_Transportation-07-512.png"
@@ -40,7 +43,7 @@ const render = new Renderer
 //     cityName: "Tel Aviv",
 //     date: "01/01/2019",
 //     price: 34,
-//     flightDuration:4.6,
+//     flightDuration: 4.6,
 //     temp: 30,
 //     condition: "cloudy",
 //     conditionPic: "https://cdn1.iconfinder.com/data/icons/airplane-glyph/64/Airplane_Flight_Airport_Transportation-07-512.png"
@@ -49,7 +52,7 @@ const render = new Renderer
 //     cityName: "Tel Aviv",
 //     date: "01/01/2019",
 //     price: 34,
-//     flightDuration:4.6,
+//     flightDuration: 4.6,
 //     temp: 30,
 //     condition: "cloudy",
 //     conditionPic: "https://cdn1.iconfinder.com/data/icons/airplane-glyph/64/Airplane_Flight_Airport_Transportation-07-512.png"
@@ -58,7 +61,7 @@ const render = new Renderer
 //     cityName: "Tel Aviv",
 //     date: "01/01/2019",
 //     price: 34,
-//     flightDuration:4.6,
+//     flightDuration: 4.6,
 //     temp: 30,
 //     condition: "cloudy",
 //     conditionPic: "https://cdn1.iconfinder.com/data/icons/airplane-glyph/64/Airplane_Flight_Airport_Transportation-07-512.png"
@@ -66,14 +69,18 @@ const render = new Renderer
 // console.log(a)
 // render.renderSearchResults(a)
 
+// // check saved
 let b = [{
-            fromCity: "Haifa",
-            fromDate: "10/11/2019",
-            toDate: "20/11/2019",
-            fromTemp: 25,
-            toTemp: 30,
-            price: 3000,
-            duration: 5.5
+    id: 37465,
+    fromCity: "Haifa",
+    fromDate: "10/11/2019",
+    toDate: "20/11/2019",
+    price: 3000,
+    duration: 5.5,
+    fromTemp: 25,
+    toTemp: 30,
+
+
 }]
 
-render.renderSavedSearches(b)
+    render.renderSavedSearches(b)
