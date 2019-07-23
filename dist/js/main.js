5dcb5bc76d5819a06182e62bd63306027f469957
zohar pfeffer <pfzohar@gmail.com>
2019-07-23 12:27:03 +0300

    fixed input names

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,14 +2,18 @@ const logic = new Logic
 const renderer = new Renderer
 const apiManager = new APIManager
 
-$('#search').on('click', async function () {
-    const fromCity = $('#fromCity')
-    const fromDate = $('#fromDate')
-    const toDate = $('#toDate')
-    const fromTemp = $('#fromTemp')
-    const toTemp = $('#toTemp')
+$('#search-btn').on('click', async function () {
+    const fromCity = $('#from-city')
+    const fromDate = $('#from-date')
+    const toDate = $('#to-date')
+    const fromTemp = $('#from-temp')
+    const toTemp = $('#to-temp')
     const price = $('#price')
-    const flightDuration = $('#flightDuration')
+    const flightDuration = $('#flight-duration')
     const matchingFlights = await logic.getSearchResults(fromCity, fromDate, toDate, fromTemp, toTemp, price, flightDuration)
     renderer.renderSearchResults(matchingFlights)
+});
+
+$('#save-search-btn').on('click', function () {
+    
 });
