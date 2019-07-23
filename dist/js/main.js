75b52e816a12c9b7111ff74621b819e2fe8b12f9
zohar pfeffer <pfzohar@gmail.com>
2019-07-23 12:42:24 +0300

    call logic.saveSearch on save seaches btn press

--- dist/js/main.js
+++ dist/js/main.js
@@ -2,16 +2,16 @@ const logic = new Logic
 const renderer = new Renderer
 const apiManager = new APIManager
 
-$('#search-btn').on('click', async function () {
-    const fromCity = $('#from-city')
-    const fromDate = $('#from-date')
-    const toDate = $('#to-date')
-    const fromTemp = $('#from-temp')
-    const toTemp = $('#to-temp')
-    const price = $('#price')
-    const flightDuration = $('#flight-duration')
-    const inputs = [fromCity, fromDate, toDate, fromTemp, toTemp, price, flightDuration]
+const fromCity = $('#from-city')
+const fromDate = $('#from-date')
+const toDate = $('#to-date')
+const fromTemp = $('#from-temp')
+const toTemp = $('#to-temp')
+const price = $('#price')
+const flightDuration = $('#flight-duration')
+const inputs = [fromCity, fromDate, toDate, fromTemp, toTemp, price, flightDuration]
 
+$('#search-btn').on('click', async function () {
     const emptyInputs = inputs.filter(i => i.val() == '')
 
     if(emptyInputs.length){
@@ -20,5 +20,10 @@ $('#search-btn').on('click', async function () {
         const matchingFlights = await logic.getSearchResults(...inputs)
         renderer.renderSearchResults(matchingFlights)
     }
+});
+
+$('#save-search-btn').on('click', function () {
+    const inputsValues = inputs.map(i => i.val())
+    logic.saveSearch(...inputsValues)
 });
 
