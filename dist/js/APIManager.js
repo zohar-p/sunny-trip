21f9355d658c4dea899fecb6c3a1f8bf3252c46f
zohar pfeffer <pfzohar@gmail.com>
2019-07-23 10:54:12 +0300

    added get flights to main logic and apiManager

--- dist/js/APIManager.js
+++ dist/js/APIManager.js
@@ -1,3 +1,21 @@
 class APIManager {
+
+    getSearchResults(fromCity, fromDate, toDate, fromTemp, toTemp, price, flightDuration){
+        return this.useAjax('get', `/flights/${fromCity}/${fromDate}/${toDate}/${fromTemp}/${toTemp}/${price}/${flightDuration}`)
+    }
+
+    useAjax(method, url, success = response=>response, data){
+        return $.ajax({
+            method,
+            url,
+            data,
+            success,
+            error: (xhr, text, err) => console.log(`
+            ERROR on $.ajax call:
+            XHR: xhr
+            Text: text
+            Error: err`)
+        })
+    }
     
 }
--- dist/js/Logic.js
+++ dist/js/Logic.js
@@ -1,3 +1,7 @@
 class Logic {
 
+    getSearchResults(fromCity, fromDate, toDate, fromTemp, toTemp, price, flightDuration){
+        return apiManager.getSearchResults(fromCity, fromDate, toDate, fromTemp, toTemp, price, flightDuration)
+    }
+
 }
